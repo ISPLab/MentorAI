997c0d6b3d86174d88a4126e929296edfb4dcd37
asimin <for_orlov@mail.ru>
2024-07-16 07:40:49 +0300

    deploy

--- assets/AboutView-BDeY2R6M copy.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,a as s}from"./index-B4fnFZ8P.js";const c={},_={class:"about"},a=s("h1",null," Универсальный и мощный инструмент преподавателя, для эффективного управления процессоми обучения учеников. ",-1),n=[a];function r(d,i){return o(),t("div",_,n)}const f=e(c,[["render",r]]);export{f as default};
