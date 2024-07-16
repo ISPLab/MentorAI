c1cf994a4035fd980deec3a386cef7a0de8fc9a2
asimin <for_orlov@mail.ru>
2024-07-16 09:07:35 +0300

    deploy

---
+++ assets/AboutView-C-5FlDTh.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as s}from"./index-Bv1uXUol.js";const c={},_={class:"about"},a=s("h1",null," Универсальный и мощный инструмент преподавателя, для эффективного управления процессоми обучения учеников. ",-1),n=[a];function r(d,i){return o(),t("div",_,n)}const f=e(c,[["render",r]]);export{f as default};
---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd ./app/
+npm run deploy
+cd ../
+git add .
+git commit -m "deploy"
+#git push origin main
