954330041872eaefe6dd5a0d96161e079d4f21ea
asimin <for_orlov@mail.ru>
2024-07-16 09:17:18 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 rm -r assets/
+cp index.html index.html.copy
 cd ./app/
 npm run deploy
 cd ../
+rm index.html
+mv index.html.copy index.html
 git add .
 echo "commit..."
 git commit -m "deploy"
 
 echo "push..."
 git push origin main
+
 
